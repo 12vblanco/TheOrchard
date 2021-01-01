77c23586c764191a446133d2b0547d28efe183cd
Victor Blanco <52953604+Rigoz13@users.noreply.github.com>
2021-01-01 19:21:29 +0000

    js update

---
+++ script.js
@@ -0,0 +1,43 @@
+const monday = document.querySelector('.opening__monday');
+monday.innerText = 'Monday Currently Closed';
+
+const tuesday = document.querySelector('.opening__tuesday');
+tuesday.innerText = 'Tuesday Currently Closed';
+
+const wednesday = document.querySelector('.opening__wednesday');
+wednesday.innerText = 'Wednesday Currently Closed';
+
+const thursday = document.querySelector('.opening__thursday');
+thursday.innerText = 'Thursday Currently Closed';
+
+const friday = document.querySelector('.opening__friday');
+friday.innerText = 'Friday Currently Closed';
+
+const saturday = document.querySelector('.opening__saturday');
+saturday.innerText = 'Saturday Currently Closed';
+
+const sunday = document.querySelector('.opening__sunday');
+sunday.innerText = 'Sunday Currently Closed';
+
+
+const monday2 = document.querySelector('.opening__monday2');
+monday2.innerText = 'Monday Currently Closed';
+
+const tuesday2 = document.querySelector('.opening__tuesday2');
+tuesday2.innerText = 'Tuesday Currently Closed';
+
+const wednesday2 = document.querySelector('.opening__wednesday2');
+wednesday2.innerText = 'Wednesday Currently Closed';
+
+const thursday2 = document.querySelector('.opening__thursday2');
+thursday2.innerText = 'Thursday Currently Closed';
+
+const friday2 = document.querySelector('.opening__friday2');
+friday2.innerText = 'Friday Currently Closed';
+
+const saturday2 = document.querySelector('.opening__saturday2');
+saturday2.innerText = 'Saturday Currently Closed';
+
+const sunday2 = document.querySelector('.opening__sunday2');
+sunday2.innerText = 'Sunday Currently Closed';
+
