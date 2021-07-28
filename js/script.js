743cd66b9c9bc6e27c946057ad54afe5f79111d2
Victor Blanco <victorblanco@ordersmadesimple.com>
2021-07-28 21:33:26 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,33 +1,34 @@
-const button = document.querySelector('button');
-const popup = document.querySelector('.popup__wrapper');
-const close = document.querySelector('.popup__content-close');
-button.addEventListener('click', () => {
-    popup.style.display = 'inline-block';
-    popup.style.margin = '0 auto';
+const button = document.querySelector("button");
+const popup = document.querySelector(".popup__wrapper");
+const close = document.querySelector(".popup__content-close");
+button.addEventListener("click", () => {
+  popup.style.display = "inline-block";
+  popup.style.margin = "0 auto";
 });
-close.addEventListener('click', () => {
-    popup.style.display = 'none';
+close.addEventListener("click", () => {
+  popup.style.display = "none";
 });
-popup.addEventListener('click', () => {
-    popup.style.display = 'none';
+popup.addEventListener("click", () => {
+  popup.style.display = "none";
 });
 
 // OPENING HOURS
-const ul = document.querySelector('.openingjs');
+const ul = document.querySelector(".openingjs");
 
 const openingjs = [
-'Monday 12:00 - 21.00 ', 
-'Tuesday 12:00 - 23:00 ', 
-'Wednesday 12:00 - 23:00 ', 
-'Thursday 12:00 - 23:00 ', 
-'Friday 12:00 - 23:00 ', 
-'Saturday 12:00 - 23:00 ', 
-'Sunday 12:00 - 21.00 '];
+  "Monday 12:00 - 23.00 ",
+  "Tuesday 12:00 - 23:00 ",
+  "Wednesday 12:00 - 23:00 ",
+  "Thursday 12:00 - 23:00 ",
+  "Friday 12:00 - 23:00 ",
+  "Saturday 12:00 - 23:00 ",
+  "Sunday 12:00 - 23.00 ",
+];
 
 let html = ``;
 
-openingjs.forEach( openingHours => {
-    html += `<li style="list-style: none; text-align: center;"> ${openingHours} </li>`
+openingjs.forEach((openingHours) => {
+  html += `<li style="list-style: none; text-align: center;"> ${openingHours} </li>`;
 });
 
 ul.innerHTML = html;
@@ -53,37 +54,30 @@ ul.innerHTML = html;
 // const sunday = document.querySelector('.footer__opening-sunday');
 // sunday.innerText = 'Sunday 12:00-09.00pm';
 
-
 // NOW OPENED?
 var d = new Date();
 var n = d.getDay();
 var now = d.getHours() + "." + d.getMinutes();
 var weekdays = [
-    ["Sunday", 12.00, 21.00],
-    ["Monday", 12.00, 21.00],
-    ["Tuesday", 12.00, 22.30],
-    ["Wednesday", 12.00, 22.30],
-    ["Thursday", 12.00, 22.30],
-    ["Friday", 12.00, 22.30],
-    ["Saturday", 12.00, 22.30],
-
+  ["Sunday", 12.0, 23.0],
+  ["Monday", 12.0, 23.0],
+  ["Tuesday", 12.0, 23.0],
+  ["Wednesday", 12.0, 23.0],
+  ["Thursday", 12.0, 23.0],
+  ["Friday", 12.0, 23.0],
+  ["Saturday", 12.0, 23.0],
 ];
 var day = weekdays[n];
 
-
-if (now > day[1] && now < day[2] || now > day[3] && now < day[4]) {
-    const opening = document.querySelector('.navbar__info-openStatus');
-    opening.innerText = 'We are open right now!';
-    // opening.innerText = 'We plan to reopen on the 29th of April!'
-
+if ((now > day[1] && now < day[2]) || (now > day[3] && now < day[4])) {
+  const opening = document.querySelector(".navbar__info-openStatus");
+  opening.innerText = "We are open right now!";
+  // opening.innerText = 'We plan to reopen on the 29th of April!'
+} else {
+  const opening = document.querySelector(".navbar__info-openStatus");
+  opening.innerText = "Sorry we are closed";
 }
-else {
-    const opening = document.querySelector('.navbar__info-openStatus');
-    opening.innerText = 'Sorry we are closed'
-        ;
-}
-
 
 // COPYRIGHT
 
-document.querySelector('.year').innerHTML = (new Date().getFullYear());
+document.querySelector(".year").innerHTML = new Date().getFullYear();
